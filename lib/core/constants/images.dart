b3c6792c762f1b04204950df136cf8580102b2aa
Arif <mdarifhosain214@gmail.com>
2025-10-20 12:04:53 +0600

    splash screen animation added done

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -22,7 +22,8 @@ class AppImages {
   static const String onboadingImg_3 = "$base/Layer_3.png";
   static const String shippingPng = "$base/shipping.png";
   static const String worldPng = "$base/world.png";
-  static const String boxPng = "$base/box.png";
+  static const String boxPng = "$base/box.png";//path245.png
+  static const String path245 = "$base/path245.png";//path245.png
 
 
   
